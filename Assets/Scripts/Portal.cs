5dfae0b5ca281c0457d8e0ba9f247c7afa7f7ef3
Jose <jose_luis.am.92@hotmail.com>
2021-03-18 15:45:35 +0100

    Interactable and enviroment

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : Collidable
+{
+    public string[] sceneNames;
+    protected override void OnCollide(Collider2D collider)
+    {
+       if(collider.name == "Player")
+        {
+            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
