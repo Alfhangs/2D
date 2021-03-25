a3181c9112222d04bfa58e80056f2a1f35b29a14
Jose <jose_luis.am.92@hotmail.com>
2021-03-25 15:56:48 +0100

    enemy and "map"

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class Mover : Fighter
 {
-    BoxCollider2D boxCollider;
+    protected BoxCollider2D boxCollider;
     Vector3 moveDelta;
     RaycastHit2D hit;
     protected float ySpeed = 0.75f;
@@ -23,6 +23,12 @@ public abstract class Mover : Fighter
             transform.localScale = Vector3.one;
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
+
+        //Add push vector, if any
+        moveDelta += pushDirection;
+
+        //Reduce push force every frame, based off recovery speed
+        pushDirection = Vector3.Lerp(pushDirection, Vector3.zero, pushRecoverySpeed);
 
         //Make sure we can move in this direction, by castin a box there first if the box returns null, we're free to move
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
