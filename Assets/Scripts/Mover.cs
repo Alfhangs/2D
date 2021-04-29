30d77533f1baf49ca91da2c83135bc9121b12b15
Jose <jose_luis.am.92@hotmail.com>
2021-04-29 17:33:41 +0200

    finish code

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -24,7 +24,7 @@ public abstract class Mover : Fighter
         if (moveDelta.x > 0)
             transform.localScale = originalSize;
         else if (moveDelta.x < 0)
-            transform.localScale = new Vector3(originalSize.x -1, originalSize.y, originalSize.z);
+            transform.localScale = new Vector3(originalSize.x * -1, originalSize.y, originalSize.z);
 
         //Add push vector, if any
         moveDelta += pushDirection;
